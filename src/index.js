0566fb57ad3f95c1df3205268a7e394b20f3fe5a
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2026-02-17 08:08:11 +0000

    add items to menu card and adjust font size

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import {generateMenu} from "./menu/menu.js";
 import {generateAbout} from "./about/about.js";
 
 generateHome();
-
 menu.addEventListener('click', generateMenu);
 about.addEventListener('click', generateAbout);
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -16,26 +16,52 @@ function generateMenu(){
     const menuCard = document.createElement('div');
     menuCard.classList.add("menu");
 
-    const item1 = document.createElement('div');
     const img1 = document.createElement('img');
     img1.src = padthai;
-    item1.classList.add("item");
-    item1.append(img1);
+    menuCard.append(img1);
 
-    const item2 = document.createElement('div');
-    const img2 = document.createElement('img');
-    img2.src = pancakes;
-    item2.classList.add("item");
-    item2.append(img2);
+    const food1 = document.createElement('p');
+    food1.textContent = `Lorem ipsum`;
+    const price1 = document.createElement('p');
+    price1.textContent = `₹1000`;
 
-    const item3 = document.createElement('div');
-    const img3 = document.createElement('img');
-    img3.src = beverage;
-    item3.classList.add("item");
-    item3.append(img3);
+    const item1 = document.createElement('div');
+    item1.classList.add("menu-item");
+    item1.append(food1);
+    item1.append(price1);
 
     menuCard.append(item1);
+
+    const img2 = document.createElement('img');
+    img2.src = pancakes;
+    menuCard.append(img2);
+
+    const food2 = document.createElement('p');
+    food2.textContent = `Lorem ipsum`;
+    const price2 = document.createElement('p');
+    price2.textContent = `₹1000`;
+
+    const item2 = document.createElement('div');
+    item2.classList.add("menu-item");
+    item2.append(food2);
+    item2.append(price2);
+
     menuCard.append(item2);
+
+    const img3 = document.createElement('img');
+    img3.src = beverage;
+    menuCard.append(img3);
+
+    const food3 = document.createElement('p');
+    food3.textContent = `Lorem ipsum`;
+    const price3 = document.createElement('p');
+    price3.textContent = `₹1000`;
+
+    const item3 = document.createElement('div');
+    item3.classList.add("menu-item");
+    item3.append(food3);
+    item3.append(price3);
+
     menuCard.append(item3);
 
     innerContent.append(menuCard);
